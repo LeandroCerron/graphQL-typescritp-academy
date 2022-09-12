9a8a26d5cb5c55c5a67768c481eed0d8b2c3da5a
Leandro <leandro_cerron@hotmail.com>
2022-09-11 22:28:10 -0300

    server

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import { createServer } from 'http';
+import { ApolloServer } from 'apollo-server-express';
+import schema from './schema';
+import expressPlayGround from 'graphql-playground-middleware-express';
+const main = async()=>{
+    try {
+        const app = express();
+
+        app.use(cors());
+        app.use(compression());
+        
+        const server = new ApolloServer({
+            schema,
+            introspection: true
+        });
+        await server.start()
+        server.applyMiddleware({app});
+        app.get('/', expressPlayGround({
+            endpoint: '/graphql'
+        }));
+        
+        console.clear();
+        const port = process.env.PORT || 3000;
+        const httpServer = createServer(app);
+        httpServer.listen(port, ()=>{
+            console.log(`http://localhost:${port}`);
+        });
+    
+    } catch (err) {
+        console.log(err);
+        let tries = 0;
+        const interval = setInterval(()=>{
+            tries++;
+            console.log(tries);
+            if (tries >= 3) {
+                clearInterval(interval);
+                process.exit(1);
+            }
+            main();
+        }, 3000)
+    }
+};
+main();
